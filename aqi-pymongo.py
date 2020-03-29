fa4c49263f95b51fc4bc7a25fc5602bc7103ab4a
soycoder <43911669+soycoder@users.noreply.github.com>
2020-03-29 15:56:49 +0700

    update aqi-pymongo.py

--- aqi-pymongo.py
+++ aqi-pymongo.py
@@ -1,2 +1,24 @@
 import pymongo
 import datetime
+
+client = pymongo.MongoClient("mongodb+srv://admin_3:*mongo3@clusterew1-uogmd.mongodb.net/test?retryWrites=true&w=majority")
+db = client.aqi-tulp
+col = db.datas
+
+def insent():
+    #someting
+
+def update():
+    #someting
+
+def delete():
+    #someting
+
+def print():
+    #someting
+
+def printAll():
+    #someting
+
+if __name__="__main__""
+    print("-init-")
